c0aeece248f2614d4f24f2f91e6a606b2f330309
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-05-02 09:20:37 -0700

    pulling latest version on git

--- app/components/CustomToolTip.js
+++ app/components/CustomToolTip.js
@@ -1,18 +1,39 @@
 import React, { PropTypes } from 'react';
 
-// componentsActiveOnGraphs
-// tooltipValues
-
 const CustomToolTip = props => {
-  const {} = props;
-  if (active) {
-    const { payload, label } = this.props;
-    return (
-      <div className="custom-tooltip">
-        <p className="label">{`${label} : ${payload[0].value}`}</p>
-        <p className="intro">{this.getIntroOfPage(label)}</p>
-        <p className="desc">Anything you want can be displayed here.</p>
-      </div>
-    );
+  const { componentsActiveOnGraphs, dataItems, getComponent } = props;
+
+  var toolTipDisplay = []; 
+  // container to hold elements to be rendered
+  // if tool tip is 'selected', push to this array
+  // time wasted = data[name]
+
+  // call getComponent, get the whole perf component back 
+  // get whatever is at index of 'label' which is a built in attribute by Recharts. plug this into the return of running getComponent
+
+  console.log("we are in CustomToolTip.js, componentsActiveOnGraphs[0]['name'] looks like", componentsActiveOnGraphs[0]['name']);
+  console.log("we are in CustomToolTip.js, componentsActiveOnGraphs[0]['metric'] looks like", componentsActiveOnGraphs[0]['metric']);
+  
+  var result = getComponent(componentsActiveOnGraphs[0]['name'],componentsActiveOnGraphs[0]['metric']);
+  console.log("we are in CustomToolTip.js, result looks like", result);
+  console.log("we are in CustomToolTip.js, result['label'] looks like", result['label']);
+
+  for (var i = 0; i < dataItems.length; i++) {
+    if (dataItems[i]['selected'] === true) {
+      toolTipDisplay.push(<p className="tool-tip-item">{dataItems[i]['label']}</p>);
+      // console.log('componentsActiveOnGraphs', componentsActiveOnGraphs[i]['metric']);
+    }
   }
+
+  for (var j = 0; j < componentsActiveOnGraphs.length; j++){
+    // console.log("we are looping through componentsActiveOnGraphs", componentsActiveOnGraphs[j]);
+  }
+
+  return (
+    <div className="custom-tooltip">
+      {toolTipDisplay}
+    </div>
+  );
 };
+
+export default CustomToolTip;
