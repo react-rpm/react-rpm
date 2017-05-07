5f87a655b7caf545f8d76389e433d8fd99027a64
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-05-06 17:58:39 -0700

    added enzyme mocha chai sinon

--- app/components/PerfItemList.js
+++ app/components/PerfItemList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import PerfItem from './PerfItem';
 
 const PerfItemList = ({ perfItems, onPerfItemClick, showDataKeys }) => (
--- app/components/ProfileDataKeyList.js
+++ app/components/ProfileDataKeyList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ProfileDataKey from './ProfileDataKey';
 
 const ProfileDataKeyList = ({ dataKeys, onDataKeyClick }) => (
